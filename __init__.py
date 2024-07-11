3344c1cefb691743de48d4d8700e24402b3d616a
Aalok Pandit <51901277+alowkii@users.noreply.github.com>
2024-07-11 21:56:20 +0530

    Changed the testing at each epoch and added a progress bar for each progress while training.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-# Usage:
-# Initialize NaiveBayesLanguageIdentifier
 import random_data_selection
 import pickle
 from test import *
@@ -8,8 +6,9 @@ import threading
 import itertools
 import sys
 import time
+from tqdm import tqdm
 
-model = NaiveBayesLanguageIdentifier(n=3)
+model = NaiveBayesLanguageIdentifier(n=20)
 epochs = 10
 
 def loading_screen(stop_event):
@@ -18,7 +17,7 @@ def loading_screen(stop_event):
         sys.stdout.write(f"\rLoading... {next(spinner)}                    ")
         sys.stdout.flush()
         time.sleep(0.1)
-    sys.stdout.write('\rModel Loaded!                                   \n')
+    sys.stdout.write('\rData Loaded!                                     \n')
     sys.stdout.flush()
 
 for i in range(epochs):
@@ -28,13 +27,19 @@ for i in range(epochs):
     loading_thread = threading.Thread(target=loading_screen, args=(stop_event,))
     loading_thread.start()
     
+    # Load data and preprocess
     random_data_selection.random_data_selection(size=2000000)
     text_data, labels = model.load_data('dataset/train-subDataSet.csv')
-    model.train(text_data, labels)
-
+    
+    # Stop the loading screen
     stop_event.set()
     loading_thread.join()
-    test(model)
+    
+    # Training process with the progress bar
+    with tqdm(total=len(text_data), desc="Training", unit="batch") as pbar:
+        model.train(text_data, labels, progress_bar=pbar)
+
+    ## test(model)
 
 print("\nTraining completed.\n")
 
